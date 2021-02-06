c3650ec15c5f635025a79189c4a3c307e4ab6ceb
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-02-05 16:55:55 -0800

    update userinfo, working on header

--- src/main/java/com/nikhilsmehta/discord6th/events/utilities/UserInfo.java
+++ src/main/java/com/nikhilsmehta/discord6th/events/utilities/UserInfo.java
@@ -49,7 +49,7 @@ public class UserInfo extends ListenerAdapter {
                 User target = event.getMessage().getMentionedUsers().get(0);
                 Member memb = event.getMessage().getMentionedMembers().get(0);
 
-                info.setTitle(memb.getNickname()+"'s Info!");
+                info.setTitle(memb.getEffectiveName()+"'s Info!");
 
                 info.setDescription (target.getAsMention()+" has been a member of discord since "
                         +target.getTimeCreated().getDayOfMonth()+" "
