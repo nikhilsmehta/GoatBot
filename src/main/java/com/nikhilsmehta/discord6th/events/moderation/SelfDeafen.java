71d89db9e77d50427db6da287e966215b1ef6298
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-02-18 15:56:22 -0800

    worked on self deafen

--- src/main/java/com/nikhilsmehta/discord6th/events/moderation/SelfDeafen.java
+++ src/main/java/com/nikhilsmehta/discord6th/events/moderation/SelfDeafen.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.api.utils.concurrent.Task;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,6 +24,8 @@ public class SelfDeafen extends ListenerAdapter {
 
             String target = event.getAuthor().getId();
             Role role = event.getGuild().getRoleById("811751764856537098"); //811751764856537098
+            List<Role> roles = event.getMember().getRoles();
+            event.getGuild().removeRoleFromMember(target, roles.get(0)).queue();
             event.getGuild().addRoleToMember(target, role).queue();
             String mins = args[1];
             long minsLong = Long.parseLong(mins)*60000;
@@ -30,6 +33,7 @@ public class SelfDeafen extends ListenerAdapter {
             TimerTask task = new TimerTask() {
                 public void run() {
                     event.getGuild().removeRoleFromMember(target, role).queue();
+                    event.getGuild().addRoleToMember(target, roles.get(0)).queue();
                 }
             };
             Timer timer = new Timer("Timer");
