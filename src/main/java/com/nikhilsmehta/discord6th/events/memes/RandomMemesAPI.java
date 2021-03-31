61b107783b4ae4f45914da18d4f9243848e28c6c
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-03-31 13:54:25 -0700

    meme updates

--- src/main/java/com/nikhilsmehta/discord6th/events/memes/RandomMemesAPI.java
+++ src/main/java/com/nikhilsmehta/discord6th/events/memes/RandomMemesAPI.java
@@ -1,7 +1,6 @@
 package com.nikhilsmehta.discord6th.events.memes;
 
 import com.nikhilsmehta.discord6th.TutorialBot;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -29,34 +28,29 @@ public class RandomMemesAPI extends ListenerAdapter {
 //test
         JSONParser parser = new JSONParser();
         String url = "";
-        String postLink = "";
 
         if (args[0].equalsIgnoreCase(TutorialBot.prefix + "meme")) {
 
             try {
                 URL memeURL = new URL("https://meme-api.herokuapp.com/gimme");
-                BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(memeURL.openConnection().getInputStream()));
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(memeURL.openConnection().getInputStream()));
 
                 String lines;
-                while ((lines = bufferedReader.readLine()) != null){
+                while ((lines = bufferedReader.readLine()) != null) {
                     JSONArray array = new JSONArray();
                     array.add(parser.parse(lines));
 
-                    for (Object o : array){
+                    for (Object o : array) {
                         JSONObject jsonObject = (JSONObject) o;
 
                         url = (String) jsonObject.get("url");
-                        postLink = (String) jsonObject.get("postLink");
                     }
                 }
 
                 bufferedReader.close();
 
-                EmbedBuilder m = new EmbedBuilder();
-                m.setTitle("Meme on r/meme");
-                m.setDescription(url);
-                m.appendDescription(" Want to see more? Click here: "+postLink);
-                m.setColor(TutorialBot.embedColor);
+                MessageBuilder m = new MessageBuilder();
+                m.setContent(url);
                 event.getChannel().sendTyping().queue();
                 event.getChannel().sendMessage(m.build()).queue();
                 m.clear();
@@ -68,6 +62,5 @@ public class RandomMemesAPI extends ListenerAdapter {
 
         }
     }
-
 
 }
