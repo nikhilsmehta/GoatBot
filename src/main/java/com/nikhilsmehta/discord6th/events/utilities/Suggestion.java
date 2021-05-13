fd3c16e4f233c4ddde2d1c9a19cb129b20d427da
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-05-13 09:31:33 -0700

    updates

--- src/main/java/com/nikhilsmehta/discord6th/events/utilities/Suggestion.java
+++ src/main/java/com/nikhilsmehta/discord6th/events/utilities/Suggestion.java
@@ -14,6 +14,13 @@ public class Suggestion extends ListenerAdapter {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
 
         if (args[0].equalsIgnoreCase(TutorialBot.prefix + "suggestion")) {
+            if(event.getAuthor().getId() == "590607487099469826"){
+                User maker = event.getJDA().getUserById("590607487099469826");
+                maker.openPrivateChannel()
+                        .flatMap(channel -> channel.sendMessage("shut yo skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome dimmadome genome full blown monochrome student loan indiana jones overgrown flintstone x and y hormone friend zone sylvester stallone sierra leone autozone professionally seen silver patrone ching chong ling long suck my ding dong head ass pubg fortnite flip phone remote control autism down syndrome stage four terminal brain cancer o'reilly auto parts silver bronze ash amiibo uv light pen sushi ramen harrison ford gamer bitch ass virgin lamp thermometor lean mean string bean charlie sheen limousine canteen trampoline serpentine antihistamine wolverine submarine unclean nectarine broken gene halloween defective spleen smokescreen james dean putting green tiny peen anti vaccine aquamarine eugene extra green nicotine vaseline jellybean magazine protein lightning-mcqueen vending machine what'chu mean iim not palnegod head ass tf up bitch\n"))
+                        .queue();
+            }
+
             EmbedBuilder e =new EmbedBuilder();
             String raw = event.getMessage().getContentDisplay();
             String yeet = raw.replace("?suggestion", "");
